e113e5437cd875226cf9386a9c73cb49b85e563b
b0ggyb33 <b0ggyb33@gmail.com>
2016-02-01 18:00:20 +0000

    added script to crop images

---
+++ autoCrop.py
@@ -0,0 +1,36 @@
+import Image
+import numpy as np
+import sys
+
+im=np.asarray(Image.open(sys.argv[1]))
+
+minx=miny=0
+for i in xrange(im.shape[0]):
+	if im[i].sum()>0:
+		minx=i
+	   	break
+
+
+for i in xrange(im.shape[1]):
+	if im[:,i].sum()>0:
+		miny=i
+	   	break
+maxx,maxy=im.shape
+for i in xrange(im.shape[0]-1,0,-1):
+	if im[i].sum()>0:
+		maxx=i
+	   	break
+
+for i in xrange(im.shape[1]-1,0,-1):
+	if im[:,i].sum()>0:
+		maxy=i
+	   	break
+
+im= im.astype('uint8') * 255
+im = 255 - im 
+
+print sys.argv[2]
+print "offset: "+str(minx)+", "+str(miny)
+print "size: "+str(maxx+1-minx)+", "+str(maxy+1-miny)
+outIm=Image.fromarray(im[minx:maxx+1,miny:maxy+1], 'L')
+outIm.save(sys.argv[2])
